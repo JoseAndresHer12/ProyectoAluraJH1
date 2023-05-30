df5f889ef2372042eca58eaac34d9d524d933e2a
Jose <josehergil72@gmail.com>
2023-05-29 23:18:27 -0500

    actualizacion del pproyecto funcional

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-const textArea = document.querySelector(".texto-encriptar");
-const mensaje = document.querySelector(".textoResultado");
+const textArea = document.querySelector(".textoInicial");
+const mensaje = document.querySelector(".textoFinal");
+const botonCopiar = document.querySelector(".copiar");
 
-function botonEncrip() {
-    const textoEncriptado = encriptar(textArea.value)
-    mensaje.value = textoEncriptado
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+function botonEncriptar(){
+    const textoEncripta = encriptar(textArea.value)
+    mensaje.value = textoEncripta
     textArea.value = "";
     mensaje.style.backgroundImage = "none"
 }
 
-function encriptar(stringEncriptada) {
-    let baseProyecto = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncriptada = stringEncriptada.tolowerCase();
+function encriptar(stringEncriptado){
+    let matrisCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase()
 
-    for (let i = 0; i < baseProyecto.length; i++) {
-        if(stringEncriptada.includes(baseProyecto[i][0])) {
-            stringEncriptada = stringEncriptada.replaceAll(baseProyecto[i][0], baseProyecto[i][1])
+    for(let i = 0; i < matrisCodigo.length; i++){
+        if(stringEncriptado.includes(matrisCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrisCodigo[i][0], matrisCodigo[i][1])
         }
     }
-    return stringEncriptada;
+    return stringEncriptado
 }
+
+function botonDesencriptar(){
+    const textoEncripta = desencriptar(textArea.value)
+    mensaje.value = textoEncripta
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function desencriptar(stringDesencriptado){
+    let matrisCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringDesencriptado.toLowerCase()
+
+    for(let i = 0; i < matrisCodigo.length; i++){
+        if(stringDesencriptado.includes(matrisCodigo[i][0])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrisCodigo[i][1], matrisCodigo[i][0])
+        }
+    }
+    return stringDesencriptado
+}
+
